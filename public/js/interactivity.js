754ebcee4590329a51878b35352e4c3c497bb2b9
mathewchris96 <147376929+mathewchris96@users.noreply.github.com>
2024-05-10 09:07:33 +0530

    Update interactivity.js

--- public/js/interactivity.js
+++ public/js/interactivity.js
@@ -137,3 +137,4 @@ function submitJobPosting(jobData) {
 function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
+}
